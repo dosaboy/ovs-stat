660d25f0f6e97940c0d2b5473f1464b4205d8841
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-10-29 13:42:16 +0000

    Ensure cache file access is under lock

--- common.sh
+++ common.sh
@@ -8,6 +8,7 @@ bold () { echo -e "\e[1m$1\e[0m"; }
 uline () { echo -e "\e[4m$1\e[0m"; }
 
 COMMAND_CACHE_PATH=`mktemp -d --suffix -$$-cache-i-am-safe-to-delete`
+LOCKPATH=$COMMAND_CACHE_PATH/lock
 
 get_ps ()
 {
@@ -15,11 +16,13 @@ get_ps ()
     if [ -r "$sos" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.ps
         if ! [ -r "$cache" ]; then
             ps auxx > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -39,11 +42,13 @@ get_ip_addr_show ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.ip_addr
         if ! [ -r "$cache" ]; then
             ip -d address > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi    
 }
 
@@ -53,11 +58,13 @@ get_ip_link_show ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.ip_addr
         if ! [ -r "$cache" ]; then
             ip -s -d link > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi    
 }
 
@@ -67,11 +74,13 @@ get_ip_netns ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.ipnetns
         if ! [ -r "$cache" ]; then
             ip netns > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -82,11 +91,13 @@ get_ns_ip_addr_show ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.${ns}_ipnetnsipas
         if ! [ -r "$cache" ]; then
             ip netns exec $ns ip addr show > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi    
 }
 
@@ -97,11 +108,13 @@ get_ns_iptables ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.${ns}_ipnetnsipts
         if ! [ -r "$cache" ]; then
             ip netns exec $ns iptables-save > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -112,11 +125,13 @@ get_ns_ss ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.${ns}_ipnetnsss
         if ! [ -r "$cache" ]; then
             ip netns exec ${ns} ss -peaonmi > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -127,11 +142,13 @@ get_ns_netstat ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.${ns}_ipnetnsnetstat
         if ! [ -r "$cache" ]; then
             ip netns exec ${ns} netstat -W -neopa > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -142,11 +159,13 @@ get_ovs_ofctl_dump_flows ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.ofctl_dump_flows_${bridge}
         if ! [ -r "$cache" ]; then
             ovs-ofctl dump-flows $bridge > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -157,11 +176,13 @@ get_ovs_ofctl_show ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.ofctl_show_${bridge}
         if ! [ -r "$cache" ]; then
             ovs-ofctl show $bridge > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -171,11 +192,13 @@ get_ovs_vsctl_show ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.vsctl_show
         if ! [ -r "$cache" ]; then
             ovs-vsctl show > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -186,11 +209,13 @@ get_ovs_appctl_fdbshow ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.ovs-appctl_fdb.show_${bridge}
         if ! [ -r "$cache" ]; then
             ovs-appctl fdb/show ${bridge} > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -200,11 +225,13 @@ get_ovsdb_client_list_dump ()
     if [ -r "${root}sos_commands" ]; then
         cat $sos
     else
+        ( flock -e 200
         cache=$COMMAND_CACHE_PATH/cache.$$.ovsdb-client_-f_list_dump
         if ! [ -r "$cache" ]; then
             ovsdb-client -f list dump > $cache
         fi
         cat $cache
+        ) 200>$LOCKPATH
     fi
 }
 
@@ -213,25 +240,25 @@ get_ns_ip_addr_show_all ()
     #NOTE: this is a bit of a hack to make sos version look the same as real
     sos=${root}sos_commands/networking/ip_netns_exec_*_ip_address_show
     cache=$COMMAND_CACHE_PATH/cache.$$.all_ipnetnsipas
+    ( flock -e 200
     if [ -r "${root}sos_commands" ]; then
         if [ -r "$cache" ]; then
             cat $cache
-            return
+        else
+            readarray -t namespaces<<<"`get_ip_netns`"
+            { ((${#namespaces[@]}==0)) || [ -z "${namespaces[0]}" ]; } && return
+            for ns in "${namespaces[@]}"; do
+                # NOTE: sometimes ip netns contains (id: <id>) and sometimes it doesn't
+                ns=${ns%% *}
+                echo "netns: $ns"
+                get_ns_ip_addr_show $ns
+            done | tee $cache
         fi
-        readarray -t namespaces<<<"`get_ip_netns`"
-        { ((${#namespaces[@]}==0)) || [ -z "${namespaces[0]}" ]; } && return
-        (
-        for ns in "${namespaces[@]}"; do
-            # NOTE: sometimes ip netns contains (id: <id>) and sometimes it doesn't
-            ns=${ns%% *}
-            echo "netns: $ns"
-            get_ns_ip_addr_show $ns
-        done
-        ) > $cache
     else
         if ! [ -r "$cache" ]; then
             ip -all netns exec ip addr show > $cache
         fi
         cat $cache
     fi
+    ) 200>$LOCKPATH
 }
