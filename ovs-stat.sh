81b2514c9708bd8d5922daf9bc045911b9bdbf8a
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-04-16 13:35:28 +0100

    Add apache license headers

--- ovs-stat.sh
+++ ovs-stat.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -u
+# Copyright 2020 opentastic@gmail.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Origin: https://github.com/dosaboy/ovs-stat
 #
 # Authors:
 #  - edward.hope-morley@canonical.com
 #  - opentastic@gmail.com
-#
+
 OVS_FS_DATA_SOURCE=
 RESULTS_PATH_ROOT=
 RESULTS_PATH_HOST=
